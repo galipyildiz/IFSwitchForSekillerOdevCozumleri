90b58a6def24328505adb5e58da9fb66aaab647b
Galip Yıldız <galipyildiz9606@outlook.com>
2021-11-01 01:50:12 +0300

    Added comment lines

--- B-FOR-Yildizlar/YildizliSekiller/Form1.cs
+++ B-FOR-Yildizlar/YildizliSekiller/Form1.cs
@@ -39,9 +39,11 @@ namespace YildizliSekiller
             Sekil6(n, karakter);
             Sekil7(n, karakter);
             Sekil8(n, karakter);
+            Sekil9(n, karakter);
+            Sekil10(n, karakter);
         }
 
-       
+
 
         private string Kere(string metin, int adet)
         {
@@ -52,16 +54,49 @@ namespace YildizliSekiller
             }
             return sonuc;
         }
+        private void Sekil10(int n, string karakter)
+        {
+            int orta = (n + 1) / 2;
+            int yildizAdet, boslukAdet;
+            int genislik = n / 2 == 0 ? n - 1 : n;
+            string sonuc = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                yildizAdet = (i > orta ? n + 1 - i : i) * 2 - 1;
+                boslukAdet = (genislik - yildizAdet) / 2;
+                sonuc += Kere(new string(' ', karakter.Length), boslukAdet);
+                sonuc += Kere(karakter, yildizAdet);
+                sonuc += Kere(new string(' ', karakter.Length), boslukAdet);
+                sonuc += Environment.NewLine;
+            }
+            txtSekil10.Text = sonuc;
+        }
+        private void Sekil9(int n, string karakter)
+        {
+            string sonuc = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1 || i == n)//başlangıç ve bitiş
+                    sonuc += Kere(karakter, n);
+                else//2 yıldız şekli azaltılmış şekilde başlangıçta ve sonda yıldız
+                    sonuc += karakter + Kere(new string(' ', karakter.Length), n - 2) + karakter;
+                sonuc += Environment.NewLine;
+            }
+            txtSekil9.Text = sonuc;
+        }
         private void Sekil8(int n, string karakter)
         {
             string sonuc = "";
-            
 
             for (int i = 1; i <= n; i++)
             {
                 for (int j = 1; j <= n; j++)
                 {
-                    sonuc += (i == j || i + j == n + 1) ? karakter : new string (' ',karakter.Length);
+                    sonuc += (i == j || i + j == n + 1) ? karakter : new string(' ', karakter.Length);
+                    //1-1 yada 1+4 == 4+1 1. satır
+                    //2-2 yada 2+3 == 4+4 2. satır
                 }
                 sonuc += Environment.NewLine;
             }
@@ -70,11 +105,11 @@ namespace YildizliSekiller
         }
         private void Sekil7(int n, string karakter)
         {
-            string sonuc = "";
+            string sonuc = "";//n-i kadar boşluk azalan
 
             for (int i = 1; i <= n; i++)
             {
-                sonuc += Kere(new string(' ',karakter.Length), n - i);
+                sonuc += Kere(new string(' ', karakter.Length), n - i);
                 sonuc += karakter;
                 sonuc += Environment.NewLine;
             }
@@ -83,10 +118,10 @@ namespace YildizliSekiller
         }
         private void Sekil6(int n, string karakter)
         {
-            string sonuc = "";
+            string sonuc = "";//i kadar boşluk
             for (int i = 0; i < n; i++)
             {
-                sonuc += Kere(new string(' ',karakter.Length), i);
+                sonuc += Kere(new string(' ', karakter.Length), i);
                 sonuc += karakter;
                 sonuc += Environment.NewLine;
             }
